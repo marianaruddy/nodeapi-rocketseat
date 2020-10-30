0f3ec9b78ec2ef87bbb77ba29ea0db0e583cc1b1
mariana.ruddy <mariana.ruddy@b2wdigital.com>
2020-10-30 11:56:38 -0300

    add update

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -15,6 +15,12 @@ module.exports = {
         return res.json(product);
     },
 
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        return res.json(product);
+    },
+
     async store(req, res) {
         const product = await Product.create(req.body);
         
