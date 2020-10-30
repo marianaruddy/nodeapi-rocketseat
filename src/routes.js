5913f8e0e378323ec700cf7d73578e18834308c3
mariana.ruddy <mariana.ruddy@b2wdigital.com>
2020-10-30 11:40:04 -0300

    add post

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const routes = Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.store);
 
 module.exports = routes;
