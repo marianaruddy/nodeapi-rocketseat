43a842d82769acb2e02e8b095e92dc46e1e93a12
mariana.ruddy <mariana.ruddy@b2wdigital.com>
2020-10-30 10:58:42 -0300

    creates product model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o App
 const app = express();
@@ -9,9 +10,17 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+requireDir('./src/models');
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
+        Product.create({
+            title: 'React Native',
+            description: 'Build native apps with react',
+            url: 'https://github.com/facebook/react-native'
+        });
+    
     return res.send('Hello world');
 });
 
